b6231cefc104dd34aaedeca8894103c6d2caee09
Mike Blackstock <mikeblackstock@gmail.com>
2012-09-12 09:48:32 -0700

    initial commit

---
+++ src/com/sensetecnic/container/HtmlCallbackActivity.java
@@ -0,0 +1,505 @@
+/**
+ * (c) 2011-2012 Sense Tecnic Systems Inc.   All rights reserved.
+ */
+
+package com.sensetecnic.container;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+public class HtmlCallbackActivity extends Activity {
+
+	public static final String URI_PREFIX = "sensetecnic://";
+	public static final String URI_SEPARATOR = "?";
+	public static final String ABORT_CODE = "!ABORT";
+	public static final String[] MODES = { "scan", "camera", "gallery", "quit", "app", "launch", "gencode", "browser" };
+
+	// request codes
+	private static final int SCAN_RQ_CODE = 0;
+	private static final int CAPTURE_IMAGE_RQ_CODE = 1;
+	private static final int CHOOSE_IMAGE_RQ_CODE = 2;
+
+	// progress dialog
+	private ProgressDialog pd;
+
+	// request params - global
+	private String callbackUrl;
+
+	// request params - camera
+	String name, tag, type, shouldPersist = "0", uploadPhotoUrl, field;
+	private File photo;
+	private long captureTime = 0;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.setContentView(R.layout.html_callback);
+
+		/* null out previous values, if any */
+		photo = null;
+		callbackUrl = null;
+		name = null;
+		tag = null;
+		type = null;
+
+		Uri data = getIntent().getData(); 
+		if (data != null) { 
+			String requestingUri = data.toString();
+			if (!requestingUri.startsWith(URI_PREFIX))
+				return;
+
+			String request = requestingUri.substring(URI_PREFIX.length());
+			int separatorIndex = request.indexOf(URI_SEPARATOR);
+
+			String mode = separatorIndex == -1 ? request : request.substring(0, separatorIndex);
+			if (!isValidMode(mode))
+				return;
+
+			try {
+				String strEntity = request.substring(separatorIndex+1);
+				final StringEntity entity = new StringEntity(strEntity, null);
+				entity.setContentType(URLEncodedUtils.CONTENT_TYPE);
+				List<NameValuePair> parameters = URLEncodedUtils.parse(entity);
+				handleRequest(mode, parameters);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Given a parsed-out mode and a list of request parameters, handle the request.
+	 * @param mode
+	 * @param parameters
+	 */
+	public void handleRequest(String mode, List<NameValuePair> parameters) {
+
+		if (mode.equals("scan")) {
+			// leech parameters out of request
+			for (NameValuePair pair : parameters) {
+				if (pair.getName().equals("ret")) {
+					callbackUrl = pair.getValue();
+				}
+				// ignore other parameters as irrelevant for this mode
+			}	
+
+			// ready to scan
+			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			startActivityForResult(intent, SCAN_RQ_CODE);
+
+		} else if (mode.equals("camera") || mode.equals("gallery")) {
+			// leech parameters out of request
+			for (NameValuePair pair : parameters) {
+				if (pair.getName().equals("ret")) 
+					callbackUrl = pair.getValue();
+				else if (pair.getName().equals("name")) 
+					name = pair.getValue();
+				else if (pair.getName().equals("tag"))
+					tag = pair.getValue();
+				else if (pair.getName().equals("type"))
+					type = pair.getValue();
+				else if (pair.getName().equals("persist"))
+					shouldPersist = pair.getValue();
+				else if (pair.getName().equals("upload_url"))
+					uploadPhotoUrl = pair.getValue();
+				else if (pair.getName().equals("field"))
+					field = pair.getValue();
+				// ignore other parameters as irrelevant for this mode
+			}		
+
+			// ready to take photo or choose it from the gallery
+			if (mode.equals("camera")) {
+				captureTime = System.currentTimeMillis();
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				photo = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+				Uri cameraFileUri = Uri.fromFile(photo); // create a file to save the image
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraFileUri); // set the image file name
+				startActivityForResult(intent, CAPTURE_IMAGE_RQ_CODE);
+			} else {
+				// we can't know about the photo object yet, because we haven't chosen anything
+				// defer assignment until later
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("image/*");
+				startActivityForResult(intent, CHOOSE_IMAGE_RQ_CODE);
+			}
+		} else if (mode.equals("quit")) {
+			// Save a flag to preferences that the HtmlChallengeActivity should finish.
+			SharedPreferences settings = getSharedPreferences("container_prefs", 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("quitChallenge", true);
+			editor.commit();
+
+			finish();
+		} else if (mode.equals("app")) {
+			String appName = null;
+			String accessToken = null;
+			for (NameValuePair pair : parameters) {
+				if (pair.getName().equals("android_activity")) { 
+					appName = pair.getValue();					
+				}
+				else if (pair.getName().equals("access_token")) {
+					accessToken = pair.getValue();
+				}
+			}
+			launchNativeApp(appName, accessToken);
+			finish();			
+		} else if (mode.equals("gencode")) {
+			String code = null;
+			String type = null;
+
+			for (NameValuePair pair : parameters) {
+				if (pair.getName().equals("type")) { 
+					type = pair.getValue();					
+				}
+				else if (pair.getName().equals("code")) {
+					code = pair.getValue();
+				}
+			}
+			generateCode(type, code);
+
+		} else if (mode.equals("browser")) {
+			String url = null;
+			for (NameValuePair pair : parameters) {
+				if (pair.getName().equals("url")) { 
+					url = pair.getValue();					
+				}
+			}
+			launchBrowser(url);
+		}
+	}
+
+	/**
+	 * Launches a native app based on the given name.  
+	 * @param appName
+	 */
+	private void launchNativeApp(String appName, String accessToken) {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setComponent(new ComponentName(appName.substring(0, appName.lastIndexOf(".")), appName));
+		intent.putExtra("accessToken", accessToken);
+		startActivity(intent);
+		finish();
+	}
+	
+	private void launchBrowser(String url) {
+		Uri uriUrl = Uri.parse(url);
+	    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);  
+	    startActivity(launchBrowser); 
+		finish();
+	}
+
+
+	private void generateCode(String type, String code) {
+		if ("upc".equalsIgnoreCase(type))
+			type = "UPC_A";
+		else if ("qr".equalsIgnoreCase(type))
+			type = "QR_CODE";
+
+		System.out.println("generating code: type=["+type+"], code=["+code+"]");
+
+		Intent intent = new Intent("com.google.zxing.client.android.ENCODE");
+
+		intent.putExtra("ENCODE_FORMAT", type);
+		intent.putExtra("ENCODE_DATA", code);
+		intent.putExtra("ENCODE_TYPE", "TEXT_TYPE"); 
+		//intent.putExtra("ENCODE_TYPE","CODE_128");
+
+		startActivity(intent);
+		finish();
+	}
+
+	/**
+	 * Handle showing mobile challenge in HTML view.
+	 * @param v
+	 */
+	public void showMobileChallenge(String challengeUrl) {
+		Intent intent = new Intent(this, HtmlContainerActivity.class);
+		intent.putExtra("url", challengeUrl);
+		startActivity(intent);
+		finish();
+	}
+
+
+
+
+	/**
+	 * Called when QR code scanning or photo taking is complete.
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		// QR code scan complete
+		if (requestCode == SCAN_RQ_CODE) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+
+				// Scan was successful.  Replace {CODE} with scan results
+				String finalUrl = callbackUrl.replaceAll("\\{code\\}", contents);
+
+				// Save callback to be refreshed in the caller version of this app
+				SharedPreferences settings = getSharedPreferences("container_prefs", 0);
+				SharedPreferences.Editor editor = settings.edit();
+				editor.putString("callbackUrl", finalUrl);
+
+				// Commit the edits!
+				editor.commit();
+
+				// start async task to post QR code activity
+				//new QRCodeActivityTask().execute(contents, "I scanned a QR code!");
+				finish();
+				
+			} else {
+				// Canceled, use abort code
+				SharedPreferences settings = getSharedPreferences("container_prefs", 0);
+				SharedPreferences.Editor editor = settings.edit();
+				editor.putString("callbackUrl", ABORT_CODE);
+
+				// Commit the edits!
+				editor.commit();
+				finish();
+			}
+		}
+
+		// Camera capture or gallery selection complete; compress and upload file to server
+		else if (requestCode == CAPTURE_IMAGE_RQ_CODE || requestCode == CHOOSE_IMAGE_RQ_CODE) {
+			if (resultCode == RESULT_OK) {
+				try {
+					pd = ProgressDialog.show(this, "", 	"Compressing photo...", true);
+					new CompressAndUploadPhotoTask().execute(requestCode == CAPTURE_IMAGE_RQ_CODE ? 
+							Uri.fromFile(photo) : intent.getData());		            
+				} catch (Exception e) {
+					finish();
+				}
+			} else {
+				finish();
+			}
+		}
+	}
+
+
+	
+	class CompressAndUploadPhotoTask extends AsyncTask<Uri, Void, Boolean> {
+		protected Boolean doInBackground(Uri... params) {
+			try {
+				InputStream imageStream = getContentResolver().openInputStream(params[0]);
+				BitmapFactory.Options options=new BitmapFactory.Options();
+				options.inSampleSize = 4;
+				Bitmap image = BitmapFactory.decodeStream(imageStream, null, options);
+				if (photo == null) {
+					photo = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+				}
+
+				int rotation = -1, rotateDegrees = 0;
+				long fileSize = photo.length();
+				Cursor mediaCursor = HtmlCallbackActivity.this.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[] {MediaStore.Images.ImageColumns.ORIENTATION, MediaStore.MediaColumns.SIZE }, MediaStore.MediaColumns.DATE_ADDED + ">=?", new String[]{String.valueOf(captureTime/1000 - 1)}, MediaStore.MediaColumns.DATE_ADDED + " desc");
+				if (mediaCursor != null && captureTime != 0 && mediaCursor.getCount() !=0 ) {
+					while(mediaCursor.moveToNext()){
+						long size = mediaCursor.getLong(1);
+						//Extra check to make sure that we are getting the orientation from the proper file
+						if(size == fileSize){
+							rotation = mediaCursor.getInt(0);
+							break;
+						}
+					}
+				}
+				if (rotation == -1) {
+					ExifInterface exif = new ExifInterface(photo.getAbsolutePath());
+					rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+					rotateDegrees = 0;
+				}
+				switch(rotation) {
+				case ExifInterface.ORIENTATION_ROTATE_90:
+					rotateDegrees-=90;
+				case ExifInterface.ORIENTATION_ROTATE_180:
+					rotateDegrees-=90;
+				case ExifInterface.ORIENTATION_ROTATE_270:
+					rotateDegrees-=90;
+				}
+				Matrix matrix = new Matrix();
+				matrix.postRotate(rotateDegrees);
+				image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), matrix, true);
+
+				FileOutputStream out = new FileOutputStream(photo);
+				image.compress(CompressFormat.JPEG, 90, out);	
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		protected void onPostExecute(Boolean result) {
+			if (result == false)
+				finish();
+
+			if (pd != null)
+				pd.dismiss();
+			pd = ProgressDialog.show(HtmlCallbackActivity.this, "", "Uploading photo...", true);
+			new PostPhotoTask().execute();
+		}
+	}
+
+
+	// uploads photo we just took to the server, and then finishes up
+	class PostPhotoTask extends AsyncTask<String, Void, String> {
+		protected String doInBackground(String... params) {
+			try {
+				String url = uploadPhotoUrl;
+
+				MultipartEntity reqEntity = new MultipartEntity();  
+
+				FileBody bin = new FileBody(photo, "image/jpg");
+				reqEntity.addPart(field, bin);
+				//reqEntity.addPart("name", new StringBody(name));
+				//reqEntity.addPart("tag", new StringBody(tag));
+				reqEntity.addPart("type", new StringBody(type));
+				reqEntity.addPart("upload", new StringBody("Upload"));
+				//reqEntity.addPart("shouldPersist", new StringBody(shouldPersist));
+
+				HttpResponse serverResponse = uploadPhoto(url, reqEntity);
+				return new BasicResponseHandler().handleResponse(serverResponse);
+			} catch (Exception e) {
+				return null;
+			}
+		}
+		protected void onPostExecute(String result) {
+			if (pd != null)
+				pd.dismiss();
+
+			if (result == null)
+				finish();
+
+			System.out.println("Uploaded to " + uploadPhotoUrl + " with response " + result);
+			
+			try {
+				JSONObject data = (JSONObject) new JSONTokener(result).nextValue();
+				int id = data.getInt("id");
+				// Scan was successful.  Replace {CODE} with scan results
+				String finalUrl = callbackUrl.replaceAll("\\{id\\}", id+"");
+
+				// Save callback to be refreshed in the caller version of this app
+				SharedPreferences settings = getSharedPreferences("container_prefs", 0);
+				SharedPreferences.Editor editor = settings.edit();
+				editor.putString("callbackUrl", finalUrl);
+
+				// Commit the edits!
+				editor.commit();
+				finish();
+
+			} catch (Exception e) {
+				finish();
+			}
+		}
+	}
+
+
+
+	/** HELPERS **/
+
+
+	/**
+	 * Helper to determine if a mode is supported by this activity.
+	 * @param mode
+	 * @return
+	 */
+	private boolean isValidMode(String mode) {
+		for (int i = 0; i < MODES.length; i++)
+			if (MODES[i].equals(mode))
+				return true;
+
+		return false;
+	}
+
+
+	public static final int MEDIA_TYPE_IMAGE = 1;
+
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type){
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(Environment.getExternalStorageDirectory() + "/meeImages");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (! mediaStorageDir.exists()){
+			if (! mediaStorageDir.mkdirs()){
+				Log.d("MEE", "Failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE){
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+					"IMG_"+ timeStamp + ".jpg");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
+	private synchronized HttpResponse uploadPhoto(String url, MultipartEntity reqEntity) {
+		// Create a new HttpClient and Post Header  
+		DefaultHttpClient httpclient = new DefaultHttpClient();  
+		HttpPost httppost = new HttpPost(url);
+		HttpResponse response = null;
+
+		try {  
+			if (reqEntity != null)
+				httppost.setEntity(reqEntity);
+
+			// Execute HTTP Post Request  
+			response = httpclient.execute(httppost);
+			System.out.println("***** RESPONSE: "+response.getStatusLine().toString());
+
+		} catch (ClientProtocolException e) {  
+			e.printStackTrace(System.out);
+		} catch (IOException e) {  
+			e.printStackTrace(System.out);
+		}
+
+		return response;
+	}
+
+}
---
+++ src/com/sensetecnic/container/HtmlContainerActivity.java
@@ -0,0 +1,270 @@
+/**
+ * (c) 2011-2012 Sense Tecnic Systems Inc.   All rights reserved.
+ */
+
+package com.sensetecnic.container;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
+
+public class HtmlContainerActivity extends Activity {
+
+	private WebView webView;
+	private String callbackUrl;
+	private String currentUrl;
+	private boolean justPlayedMedia = false;
+	private ProgressBar pbLoading;
+
+	public ProgressDialog pd; 
+	
+	private static final String OVERRIDE_PREFIX = "sensetecnic://";
+	private static final String DEFAULT_URL = "http://www.google.com";
+	
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.html_challenge);
+		pbLoading = (ProgressBar)findViewById(R.id.pbLoading);
+		webView = (WebView) findViewById(R.id.webview);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+		webView.setWebChromeClient(new ContainerWebChromeClient());
+		
+		webView.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				if (url.startsWith(OVERRIDE_PREFIX)) {
+					Intent intent = new Intent(HtmlContainerActivity.this, HtmlCallbackActivity.class);
+					intent.setData(Uri.parse(url));
+					startActivity(intent);
+					return true;
+				} 
+				else if (url.startsWith("http://www.youtube.com")) {
+					Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url)); startActivity(i);
+					return true;
+				} else if (url.startsWith("http://www.facebook.com")) {
+					Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url)); startActivity(i);
+					return true;
+				} else if (url.endsWith(".mp3") || url.endsWith(".wav")) {
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setDataAndType(Uri.parse(url), "audio/*");
+					startActivity(intent);   
+					justPlayedMedia = true;
+					return true;
+				} else if (url.endsWith(".mp4") || url.endsWith(".3gp")) {
+					Intent intent = new Intent(Intent.ACTION_VIEW); 
+					intent.setDataAndType(Uri.parse(url), "video/*");
+					startActivity(intent); 
+					justPlayedMedia = true;
+					return true;
+				}
+
+				// ordinary link
+				return false;
+			}
+
+		});
+
+		String url = getIntent().getStringExtra("url");
+		currentUrl = url;
+
+		if (url != null) {
+			if (url.startsWith(OVERRIDE_PREFIX)) {
+				Intent intent = new Intent(HtmlContainerActivity.this, HtmlCallbackActivity.class);
+				intent.setData(Uri.parse(url));
+				startActivity(intent);
+				// do we want to load a webpage from this scan?  if not, we're done.
+				if (!url.contains("ret="))
+					finish();
+			}
+			else {
+				System.out.println("Webview loading: " + url);
+				webView.loadUrl(url);
+			}
+
+		} else {
+			// load default url
+			webView.loadUrl(DEFAULT_URL);
+		}
+
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.html_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.refresh:
+			doRefresh();
+			return true;
+		case R.id.gotourl:
+			typeUrl();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void typeUrl() {
+		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this);
+		final LinearLayout layout = new LinearLayout(this);
+		final TextView instructions = new TextView(this);
+		input.setText("http://");
+		input.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+		layout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		input.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT, 1f));
+		instructions.setText("Enter URL:");
+		instructions.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 0f));
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.addView(instructions);
+		layout.addView(input);
+		alert.setView(layout);
+		alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input.getText().toString().trim();
+				webView.loadUrl(value);
+			}
+		});
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				dialog.cancel();
+			}
+		});
+		alert.show();
+	}
+
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (justPlayedMedia) {
+			justPlayedMedia = false;
+			return;
+		}
+
+		// load callback URL from preferences, if it exists, and whether the user has requested a quit.  
+		// These preferences will only exist if, while we were suspended, our sub-launched self wrote the preferences 
+		// because it wants us to refresh the page or quit.
+		SharedPreferences settings = getSharedPreferences("container_prefs", 0);
+		callbackUrl = settings.getString("callbackUrl", "");
+		boolean shouldQuit = settings.getBoolean("quitChallenge", false);
+
+		// now clear the preferences again so that we don't refresh ourselves/quit if suspended by some other
+		// activity
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("callbackUrl", "");
+		editor.putBoolean("quitChallenge", false);
+		editor.commit();
+
+		if (shouldQuit) {
+			doLeaveChallengeConfirmation();
+		}
+
+		else if (callbackUrl != null && !callbackUrl.equals("")) {
+			if (callbackUrl.equals(HtmlCallbackActivity.ABORT_CODE)) {
+				if (currentUrl == null || currentUrl.startsWith(DEFAULT_URL)) 
+					finish();
+			}
+			else {
+				System.out.println("Webview loading: " + callbackUrl);
+				webView.loadUrl(callbackUrl);
+				currentUrl = callbackUrl;
+			}
+		} 
+
+	}
+
+
+	@Override
+	public void onBackPressed() {
+		if (webView.canGoBack()) {
+			webView.goBack();
+		}
+		else {
+			doLeaveChallengeConfirmation();
+		}
+	}
+
+
+	/**
+	 * Handle page refresh
+	 */
+	private void doRefresh() {
+		webView.reload();
+	}
+
+	/**
+	 * Handle leaving the challenge via the back button or the menu option.
+	 */
+	private void doLeaveChallengeConfirmation() {
+		finish();
+	}
+
+
+
+
+	/**
+	 * Custom web chrome client so that we can grant geolocation privileges automatically and display alerts if they come up
+	 * 
+	 * @author tom
+	 *
+	 */
+	class ContainerWebChromeClient extends WebChromeClient {
+		@Override
+		public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+			callback.invoke(origin, true, false);
+		}
+
+		@Override
+		public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+			new AlertDialog.Builder(view.getContext()).setMessage(message).setCancelable(true).show();
+			result.confirm();
+			return true;
+		}
+
+		public void onProgressChanged(WebView view, int progress) 
+		{
+			if (progress < 100 && pbLoading.getVisibility() == ProgressBar.GONE) {
+				pbLoading.setVisibility(ProgressBar.VISIBLE);				
+			}
+			pbLoading.setProgress(progress);
+			if (progress == 100) {
+				pbLoading.setVisibility(ProgressBar.GONE);
+			}
+		}
+
+	}
+
+
+}
